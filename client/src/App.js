e4211c7192e7bd252f312810d7743a678ea8a89e
Joseph Gale <24618839+josephgale@users.noreply.github.com>
2020-10-16 17:55:04 -0500

    first push

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Hello Kris Erickson!</h2> --from Iowa
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+
+const app = express();
+
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname,'client/build')));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'client/build','index.html'))
+        })
+    }
+
+const port = process.env.PORT || 8000;
+app.listen(port,()=>console.log(`Server running on port ${port}`));
