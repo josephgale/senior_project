609f8302df0cd1aeddb70284f48760fbcfed0d83
Joseph Gale <24618839+josephgale@users.noreply.github.com>
2020-10-16 21:13:20 -0500

    updating db creds for heroku

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+
+//connect to db
+mongoose.connect(process.env.DB_CONNECT,{
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+.then(()=>console.log('DB Connected'))
+.catch(err=>console.log('DB Connection error',err))
 
 if(process.env.NODE_ENV==='production'){
     app.use(express.static(path.join(__dirname,'client/build')));
